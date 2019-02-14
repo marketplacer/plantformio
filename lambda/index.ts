dc8cf1f614e098a6a89342fa30cbc83104befee7
Ellis Clayton <ellis@ellisclayton.info>
2019-02-14 20:32:03 +1100

    Convert to TypeScript
    Converts the code to TypeScript, including adding tsconfig, and a linter
    preset.

---
+++ lambda/index.ts
@@ -0,0 +1,31 @@
+import metrics from 'datadog-metrics'
+import axios from 'axios'
+
+metrics.init({
+  host: 'plants',
+  prefix: 'plants.',
+  apiKey: process.env.DATADOG_API_KEY
+})
+
+interface Payload {
+  /** Sensor reading */
+  value: number
+  plantName: string
+}
+
+export const handler = ({ value, plantName }: Payload): void => {
+  metrics.gauge(`${plantName}.moisture`, value)
+  const message = `Received ${value} on ${plantName}`
+  console.log(message)
+
+  axios
+    .post(process.env.WEBHOOK_URL, {
+      text: message
+    })
+    .then(() => {
+      console.log('Succeeded')
+    })
+    .catch(e => {
+      console.error('Network error', e)
+    })
+}
